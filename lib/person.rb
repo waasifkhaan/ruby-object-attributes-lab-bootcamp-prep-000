f0ab01c80ee805e6ec80b2e94d0cb1d94120aa79
Wasif Khan <waasif.khaan@gmail.com>
2018-04-25 02:51:43 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,29 @@
 class Person
   
+  def initialize(name)
+    @name = name
+  end
   
+  def name(name)
+  @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+ 
+  
+  def initialize(job)
+    @job = job
+  end
+  
+  def job(new_job)
+  @job = new_job
+  end
+  
+  def job
+    @job
+  end
 
 end
